628434fc65ba2f9fe55d08bd6d9662a50cde6f4d
vova11 <vladimir.zembera@gmail.com>
2022-02-10 14:35:23 +0100

    button to download plgin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Coffee from './Coffee';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h1>
           Scam Alert
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </h1>
+        <button className="btn-light">
+          <a href="https://chrome.google.com/webstore/detail/scam-alert/ifpffcplkjdpdgaionjapdnodeaieeek?hl=sk&authuser=0">Chrome Plugin</a>
+        </button>
+        
       </header>
     </div>
   );
